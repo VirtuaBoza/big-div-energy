8922855016a4eff8c8d48afd8bfb2eea28638429
VirtuaBoza <andrew.m.boza@gmail.com>
2020-02-09 08:34:43 -0500

    Fixes storybook after switch to sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
@@ -15,6 +17,11 @@ module.exports = {
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
     return config;
   },
 };
