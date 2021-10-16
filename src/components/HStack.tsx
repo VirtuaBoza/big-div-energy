158d3ef306b656067676109dc68a645bfb27137c
Andrew Boza <aboza@madmobile.com>
2021-10-15 22:25:13 -0400

    get storybook workin with emotion

--- src/components/HStack.tsx
+++ src/components/HStack.tsx
@@ -1,7 +1,8 @@
+import { css } from "@emotion/react";
 import React from "react";
 
-export const HStack: React.FC = React.memo((props) => {
-  return <div {...props} />;
+export const HStack: React.VFC = React.memo((props) => {
+  return <div css={css``} {...props}></div>;
 });
 
 HStack.displayName = "HStack";
